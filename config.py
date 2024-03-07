34d0fc2e7b271b12864be3f9d800f46ca74d485e
Zholobov Oleg <69785001+parlorsky@users.noreply.github.com>
2024-03-08 01:46:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#API_ID = тут ваш апи_ид в формате int
+API_ID = 321 # тут ваш апи_ид в формате int
 API_HASH = '56652eb73b9exxxxxxxxxxxxxx708926e1'
 NAME_OF_SESSION = 'anon'
 LINK_TO_CHAT = 'https://t.me/+xxxxxxxxxxxxxxx'
